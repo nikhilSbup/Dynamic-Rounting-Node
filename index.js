a516c46da3f9389bb1e40a69eb77d7f4c5ba0495
Nikhil Kumar Singh <sing9999nikhil@gmail.com>
2025-08-02 02:01:34 +0530

    setting public static files

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const express = require('express')
 const app = express()
+const path = require('path')
 
 app.use(express.json())
 app.use(express.urlencoded({extended : true}))
+app.use(express.static(path.join(__dirname,'public')))
+app.set('view engine', 'ejs')
 
 app.get("/",function(req,res){
-    res.send("It's running...")
+    res.render("index")
 })
 
 app.listen(3000,function(){
